e8c17b5adfe604e24b6b562b5f4b5fe437251be7
dheeraj <dheerajjain004@gmail.com>
2023-10-12 06:16:35 +0100

    load job data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,15 @@ import { User } from './_models';
 // @ts-ignore
 import * as usersData from '../staticData/userData.json';
 
+// @ts-ignore
+import * as jobsData from '../staticData/jobData.json';
+
 const usersKey = 'listening-buddy-users';
 let users: any[] = JSON.parse(localStorage.getItem(usersKey)!) || [];
+
+
+const jobsKey = 'listening-buddy-jobs';
+let jobs: any[] = JSON.parse(localStorage.getItem(jobsKey)!) || [];
 
 @Component({ selector: 'app-root', templateUrl: 'app.component.html' })
 export class AppComponent {
@@ -26,6 +33,13 @@ export class AppComponent {
       if(! users.find(x => x.id === user.id as any )){
         users.push(user);
         localStorage.setItem(usersKey, JSON.stringify(users));
+      }
+    })
+
+    jobsData.default.forEach((job: { id: any; }) =>{
+      if(! jobs.find(x => x.id === job.id as any )){
+        jobs.push(job);
+        localStorage.setItem(jobsKey, JSON.stringify(jobs));
       }
     })
   }
